c2b263fa9948cc8bae6879b680e55daeb5f67199
suyinkaishifangle <597524105@qq.com>
2018-05-18 09:54:21 +0800

    奇怪的大冒险

---
+++ Assets/Game/Scripts/UIPanel/UIWin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIWin : UIBase
+{
+   
+
+    public override void DoOnEntering()
+    {
+        transform.DOLocalMoveY(0, 1f);
+    }
+
+    public override void DoOnPausing()
+    {
+        CanvasGroup.alpha = 0f;
+    }
+
+    public override void DoOnResuming()
+    {
+        CanvasGroup.alpha = 1f;
+    }
+
+    public override void DoOnExiting()
+    {
+        CanvasGroup.alpha = 0f;
+    }
+
+    public void OnRePlayClick()
+    {
+        UIManager.Instance.PopUIPanel();
+    }
+}
